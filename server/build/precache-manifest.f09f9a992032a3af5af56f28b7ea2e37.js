8d8ec09d797d186928625e8cfc0149315fa874b7
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-04-23 13:35:13 -0400

    updated heroku

---
+++ server/build/precache-manifest.f09f9a992032a3af5af56f28b7ea2e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34094443a5009343767949bcbdb574b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5af3729454f120d46bb",
+    "url": "/static/css/main.c8f649cb.chunk.css"
+  },
+  {
+    "revision": "7e2dfcbe193fbaef61b9",
+    "url": "/static/js/2.0d8239f4.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.0d8239f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5af3729454f120d46bb",
+    "url": "/static/js/main.63107bf1.chunk.js"
+  },
+  {
+    "revision": "5f94b89eedc83b6165b9",
+    "url": "/static/js/runtime-main.1fbdab42.js"
+  }
+]);
