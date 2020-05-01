6a7fde116ccee0d075e035e52b59dc254f538ea1
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-05-01 13:33:09 -0400

    updated footer

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -5,9 +5,24 @@ export default function Footer() {
     return (
       <div>
         <Menu fixed="bottom">
-          {/* <div>
-          <p>Jarrod Finn</p>
-          </div> */}
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a href="https://https://docs.google.com/document/d/1XPA8YRSIOZ76fcg6IUIq0EbJ5eA_4KfejXNcFe7l8iM/edit.google.com/open?id=1nmtOf6ClDlIzS3zN55P71jQ6zptgChhz">
+                Read my Resume
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.linkedin.com/in/jarrod-finn-41175665/">
+                LinkedIn
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="https://https://github.com/jarrodfinn.linkedin.com/in/jarrod-finn-41175665/">
+                Github
+              </a>
+            </li>
+            
+          </ul>
         </Menu>
       </div>
     );
