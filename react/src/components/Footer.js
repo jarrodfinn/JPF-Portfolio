d636d924f8549615fb66c50476ef4f09a961090d
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-05-04 18:26:01 -0400

    footer links

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <Menu fixed="bottom">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a href="https://https://docs.google.com/document/d/1XPA8YRSIOZ76fcg6IUIq0EbJ5eA_4KfejXNcFe7l8iM/edit.google.com/open?id=1nmtOf6ClDlIzS3zN55P71jQ6zptgChhz">
+              <a href="https://docs.google.com/document/d/1XPA8YRSIOZ76fcg6IUIq0EbJ5eA_4KfejXNcFe7l8iM/">
                 Read my Resume
               </a>
             </li>
@@ -17,11 +17,8 @@ export default function Footer() {
               </a>
             </li>
             <li class="nav-item">
-              <a href="https://https://github.com/jarrodfinn.linkedin.com/in/jarrod-finn-41175665/">
-                Github
-              </a>
+              <a href="https://github.com/jarrodfinn">Github</a>
             </li>
-            
           </ul>
         </Menu>
       </div>
