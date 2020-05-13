c39c8e811388320c57d20ca91d927d3e0e9dc5ae
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-05-13 13:40:08 -0400

    footer

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
-import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
       <div>
         <Menu fixed="bottom fixed">
           <Menu.Item link>
-            <Link to="https://docs.google.com/document/d/1XPA8YRSIOZ76fcg6IUIq0EbJ5eA_4KfejXNcFe7l8iM/">
+            <a href="https://docs.google.com/document/d/1XPA8YRSIOZ76fcg6IUIq0EbJ5eA_4KfejXNcFe7l8iM/">
               Read my Resume
-            </Link>
+            </a>
           </Menu.Item>
           <Menu.Item link>
-            <Link to="https://www.linkedin.com/in/jarrod-finn-41175665/">
+            <a href="https://www.linkedin.com/in/jarrod-finn-41175665/">
               LinkedIn
-            </Link>
+            </a>
           </Menu.Item>
           <Menu.Item link>
-            <Link to="/https://github.com/jarrodfinn">Github</Link>
+            <a href="https://github.com/jarrodfinn">Github</a>
           </Menu.Item>
         </Menu>
       </div>
