701023a3a280568e3762a7b7385649b5d1b9dbca
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-07-10 18:37:32 -0400

    new build

---
+++ server/build/precache-manifest.f80e0a8595b369dda473c337938d8062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac820157496619c5b48a2899b2adb346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a595411556ce541274c6",
+    "url": "/static/css/main.14aa7cc6.chunk.css"
+  },
+  {
+    "revision": "bbc5b4b02b6f4e13e38a",
+    "url": "/static/js/2.035887b9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.035887b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a595411556ce541274c6",
+    "url": "/static/js/main.42ca8b3f.chunk.js"
+  },
+  {
+    "revision": "5f94b89eedc83b6165b9",
+    "url": "/static/js/runtime-main.1fbdab42.js"
+  }
+]);
